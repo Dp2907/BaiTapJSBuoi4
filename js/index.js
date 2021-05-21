68c7f22b21d5ff89a269c4e1e535a80561cb7489
PerMees <somdp2907@gmail.com>
2021-05-21 21:52:04 +0700

    done

--- js/index.js
+++ js/index.js
@@ -28,18 +28,41 @@ var geographyMark = document.getElementById("geographyMark");
 var englishMark = document.getElementById("englishMark");
 var alertMark = document.getElementById("alertMark");
 
+function calcAvgMark(
+  _literatureMark,
+  _historyMark,
+  _geographyMark,
+  _englishMark
+) {
+  var result = (
+    (_literatureMark + _historyMark + _geographyMark + _englishMark) /
+    4
+  ).toFixed(2);
+  return result;
+}
+
 btnCalcAvgMark.onclick = function () {
   var _literatureMark = Number(literatureMark.value);
   var _historyMark = Number(historyMark.value);
   var _geographyMark = Number(geographyMark.value);
   var _englishMark = Number(englishMark.value);
-  var result = (
-    (_literatureMark +
-      _historyMark +
-      _geographyMark +
-      _englishMark +
-      _englishMark) /
-    5
-  ).toFixed(2);
-  alertMark.innerHTML = result;
+
+  if (
+    _literatureMark < 0 ||
+    _historyMark < 0 ||
+    _geographyMark < 0 ||
+    _englishMark < 0 ||
+    _literatureMark > 10 ||
+    _historyMark > 10 ||
+    _geographyMark > 10 ||
+    _englishMark > 10
+  )
+    alert("Diem phai nam trong khoang tu 0 den 10, vui long nhap lai");
+  else
+    alertMark.innerHTML = calcAvgMark(
+      _literatureMark,
+      _historyMark,
+      _geographyMark,
+      _englishMark
+    );
 };
